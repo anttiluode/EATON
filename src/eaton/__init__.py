7f967529af2e687bab28050a54b839db1da44043
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-09-23 07:04:13 +0300

    feat: export frozen world tape

--- src/eaton/__init__.py
+++ src/eaton/__init__.py
@@ -1,3 +1,10 @@
 from .core import EpisodeInput, EpisodeResult, FrozenConfig, run_episode
+from .world import build_seed_tape
 
-__all__ = ["EpisodeInput", "EpisodeResult", "FrozenConfig", "run_episode"]
+__all__ = [
+    "EpisodeInput",
+    "EpisodeResult",
+    "FrozenConfig",
+    "run_episode",
+    "build_seed_tape",
+]
